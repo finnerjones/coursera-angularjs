52f045bc9c4b5e1d536d6e4eb904f318e18786cf
finner <finnseamus@gmail.com>
2016-10-05 22:34:16 +0200

    adding categories component but not using it yet

--- week4-assignment1/categories.component.js
+++ week4-assignment1/categories.component.js
@@ -3,7 +3,24 @@
 
 angular.module('MenuApp')
 .component('categories', {
-  
-})
+  templateUrl: 'categoryList.html',
+  controller: CategoriesComponentController,
+  bindinds: {
+    categories: '<'
+  },
+  resolve: {
+    categories: ['MenuDataService', function (MenuDataService) {
+      return MenuDataService.getAllCategories();
+    }]
+  }
+});
+
+CategoriesComponentController.$inject = ['categories'];
+function CategoriesComponentController(categories) {
+  var $ctrl = this;
+
+  $ctrl.categories = categories;
+}
+
 
 })();
