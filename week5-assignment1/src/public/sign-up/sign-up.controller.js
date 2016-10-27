3483178951febf457ac6c6b382efed6137efbcb4
finner <finnseamus@gmail.com>
2016-10-27 22:58:58 +0200

    added first name on registration form and route public.signup. Lots to do yet

---
+++ week5-assignment1/src/public/sign-up/sign-up.controller.js
@@ -0,0 +1,18 @@
+(function () {
+"use strict";
+
+angular.module('public')
+.controller('SignUpController', SignUpController);
+
+SignUpController.$inject = [];
+function SignUpController() {
+  var $ctrl = this;
+  $ctrl.userDetails = {
+    firstName: undefined
+  };
+
+
+}
+
+
+})();
